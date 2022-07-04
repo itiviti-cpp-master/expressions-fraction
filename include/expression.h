96ba7511e9de8b52f54762b534633bee43e0e424
Artem Koton <kotonartem@protonmail.com>
2022-07-04 17:21:04 +0300

    Use unique_ptr for clone (and update tests)

--- include/expression.h
+++ include/expression.h
@@ -3,14 +3,15 @@
 #include "fraction.h"
 
 #include <map>
+#include <memory>
 #include <ostream>
 #include <string>
 
 struct Expression
 {
-    virtual Fraction eval(std::map<std::string, Fraction> const & values = {}) const = 0;
+    virtual Fraction eval(const std::map<std::string, Fraction> & values = {}) const = 0;
 
-    virtual Expression * clone() const = 0;
+    virtual std::unique_ptr<Expression> clone() const = 0;
 
     friend std::ostream & operator<<(std::ostream & out, const Expression & expression);
 };
